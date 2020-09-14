1671d45905e0cbb24bcf6784def056af90568732
shrey08 <shreyarora1967@gmail.com>
2020-09-14 22:41:55 +0530

    Adding route

--- react-slack/src/index.js
+++ react-slack/src/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import { App, SignIn } from "./components";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import { App, SignIn } from './components';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </BrowserRouter>,
+  document.getElementById('root')
 );

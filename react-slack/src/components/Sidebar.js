a36700d2f4c20ca87dae176bfa5d6f16e4eab0fb
shrey08 <shreyarora1967@gmail.com>
2020-09-14 23:03:29 +0530

    Sidebar component edited

--- react-slack/src/components/Sidebar.js
+++ react-slack/src/components/Sidebar.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 
 class Sidebar extends Component {
   render() {
-    return <div>Sidebar</div>;
+    return (
+      <div id="sidebar">
+        <div className="user-profile">
+          <div>
+            <img />
+          </div>
+          <div>Ronald</div>
+        </div>
+        <div className="user-channels"></div>
+      </div>
+    );
   }
 }
 
